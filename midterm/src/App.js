64bb7c704374c53bf07ef8210884d734660f05b4
Damir <mrdnigel@gmail.com>
2023-10-28 15:53:33 +0600

    Post

--- midterm/src/App.js
+++ midterm/src/App.js
@@ -1,7 +1,7 @@
 import {createBrowserRouter,createRoutesFromElements,Route,Outlet,RouterProvider } from 'react-router-dom'
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
-import Posts from "./pages/posts/Posts";
+import Post from "./pages/post/Post";
 import Footer from "./pages/footer/footer";
 import Navbar from "./pages/navbar/navbar";
 
@@ -10,7 +10,7 @@ function App() {
       createRoutesFromElements(
           <Route path='/' element={<Root />}>
             <Route index element={<Home />} path='/'/>
-            <Route element={<Posts />} path='/posts'/>
+            <Route element={<Post />} path='/posts'/>
             <Route element={<Profile />} path='/profile'/>
           </Route>
       ), )
--- midterm/src/pages/navbar/navbar.jsx
+++ midterm/src/pages/navbar/navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
     return (
         <nav >
             <div className={classes.nav}>
+                <h2>Twitter</h2>
                 <Link className={classes.navItems} to="/"><h2>Home</h2></Link>
-                <Link className={classes.navItems} to="/posts"><h2>Posts</h2></Link>
                 <Link className={classes.navItems} to="/profile"><h2>Profile</h2></Link>
             </div>
         </nav>
